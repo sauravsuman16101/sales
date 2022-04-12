bb92f9928d62192b57590f2a07fc4309f4c8ea54
Saurav Suman <sauravchandela95@gmail.com>
2022-04-12 14:09:13 +0530

    Sales Example

---
+++ src/io/calculatecost/ItemSpecification.java
@@ -0,0 +1,57 @@
+package io.calculatecost;
+
+public class ItemSpecification
+{
+    private String name;
+    private double price;
+    private int quantity;
+    private String type;
+
+    public ItemSpecification(String name, double price, int quantity, String type)
+    {
+        this.setName(name);
+        this.setPrice(price);
+        this.setQuantity(quantity);
+        this.setType(type);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+}
---
+++ src/io/calculatecost/TaxCalculation.java
@@ -0,0 +1,55 @@
+package io.calculatecost;
+
+public class TaxCalculation
+{
+    public static double calculateTax(String type, double price)
+    {
+        if (type.equals("Raw"))
+        {
+            return getRawTypeTax(price);
+        }
+        else if (type.equals("Manufactured"))
+        {
+            return getManufacturedTypeTax(price);
+        }
+        else if (type.equals("Imported"))
+        {
+            return getImportedTypeTax(price);
+        }
+        return 0;
+    }
+
+    public static double getRawTypeTax(double price)
+    {
+        double totalTax = 12.5 * (price / 100);
+        return totalTax;
+    }
+
+    public static double getManufacturedTypeTax(double price)
+    {
+        double totalTax = getRawTypeTax(price) + 2 * ((getRawTypeTax(price) + price) / 100);
+        return totalTax;
+    }
+
+    public static double getImportedTypeTax(double price)
+    {
+        double importDuty = 10 * (price / 100);
+        double totalTax;
+        double surcharge = 0;
+        if ((importDuty + price) <= (double) 100)
+        {
+            surcharge = 5;
+        }
+        else if (price > (double) 100 && price <= (double) 200)
+        {
+            surcharge = 10;
+        }
+        else if (price > 200)
+        {
+            surcharge = 5 * (price / 100);
+        }
+
+        totalTax = importDuty + surcharge;
+        return totalTax;
+    }
+}
